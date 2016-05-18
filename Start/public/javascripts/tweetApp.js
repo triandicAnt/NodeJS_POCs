feaffcc2135ea4878ba8d41c844498811d6e5afc
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-17 20:34:31 -0400

    integrated front end

--- Start/public/javascripts/tweetApp.js
+++ Start/public/javascripts/tweetApp.js
@@ -1,45 +1,78 @@
-var app = angular.module('tweetApp',['ngRoute']);
-console.log('I m getting called');
-// configure routing
+var app = angular.module('tweetApp', ['ngRoute', 'ngResource']).run(function($rootScope) {
+	$rootScope.authenticated = false;
+	$rootScope.current_user = '';
+	
+	$rootScope.signout = function(){
+    	$http.get('auth/signout');
+    	$rootScope.authenticated = false;
+    	$rootScope.current_user = '';
+	};
+});
+
 app.config(function($routeProvider){
-    $routeProvider
-    .when('/',{
-        templateUrl:'main.html',
-        controller: 'mainController'
-    })
-    .when('/login',{
-        templateUrl:'login.html',
-        controller:'authController'
-    })
-    .when('/register',{
-        templateUrl: 'register.html',
-        controller: 'authController'
+	$routeProvider
+		//the timeline display
+		.when('/', {
+			templateUrl: 'main.html',
+			controller: 'mainController'
+		})
+		//the login display
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'authController'
+		})
+		//the signup display
+		.when('/signup', {
+			templateUrl: 'register.html',
+			controller: 'authController'
+		});
+});
+
+app.factory('postService', function($resource){
+	return $resource('/api/posts/:id');
+});
+
+app.controller('mainController', function(postService, $scope, $rootScope){
+	$scope.posts = postService.query();
+	$scope.newPost = {created_by: '', text: '', created_at: ''};
+	
+	$scope.post = function() {
+	  $scope.newPost.created_by = $rootScope.current_user;
+	  $scope.newPost.created_at = Date.now();
+	  postService.save($scope.newPost, function(){
+	    $scope.posts = postService.query();
+	    $scope.newPost = {created_by: '', text: '', created_at: ''};
+	  });
+	};
+});
+
+app.controller('authController', function($scope, $http, $rootScope, $location){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    $http.post('/auth/login', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
     });
-});
+  };
 
-// mainController to control main 
-app.controller('mainController',function($scope){
-    $scope.posts = [];
-    $scope.newPost = {created_by:'', text:'',created_at:''}; 
-
-    $scope.post = function(){
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by:'', text:'',created_at:''};
-    };
-
-});
-
-// authController for login and register
-
-app.controller('authController',function($scope){
-    $scope.user = {username:'',password:''};
-    $scope.error_message = '';
-    
-    $scope.login = function () {
-        $scope.error_message = 'Login request for '+ $scope.user.username;
-    };
-    $scope.register = function(){
-        $scope.error_message = 'Registration request for '+$scope.user.username;
-    };
+  $scope.register = function(){
+    $http.post('/auth/signup', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
+  };
 });
--- Start/routes/api.js
+++ Start/routes/api.js
@@ -2,100 +2,89 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
 var Post = mongoose.model('Post');
-
 //Used for routes that must be authenticated.
 function isAuthenticated (req, res, next) {
-    // if user is authenticated in the session, call the next() to call the next request handler 
-    // Passport adds this method to request object. A middleware is allowed to add properties to
-    // request and response objects
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
 
-    //allow all get request methods
-    if(req.method === "GET"){
-        return next();
-    }
-    if (req.isAuthenticated()){
-        return next();
-    }
+	//allow all get request methods
+	if(req.method === "GET"){
+		return next();
+	}
+	if (req.isAuthenticated()){
+		return next();
+	}
 
-    // if the user is not authenticated then redirect him to the login page
-    return res.redirect('/#login');
+	// if the user is not authenticated then redirect him to the login page
+	return res.redirect('/#login');
 };
 
 //Register the authentication middleware
 router.use('/posts', isAuthenticated);
 
-
-//api for all posts
 router.route('/posts')
+	//creates a new post
+	.post(function(req, res){
 
-    //create a new post
-    .post(function(req, res){
+		var post = new Post();
+		post.text = req.body.text;
+		post.created_by = req.body.created_by;
+		post.save(function(err, post) {
+			if (err){
+				return res.send(500, err);
+			}
+			return res.json(post);
+		});
+	})
+	//gets all posts
+	.get(function(req, res){
+		console.log('debug1');
+		Post.find(function(err, posts){
+			console.log('debug2');
+			if(err){
+				return res.send(500, err);
+			}
+			return res.send(200,posts);
+		});
+	});
 
-        //create a new post in the database
-        var post = new Post();
-        post.text = req.body.text;
-        post.created_by = req.body.created_by;
-        post.save(function(err,post){
-            if(err){
-                return res.send(500,err);
-            }
-            return res.json(post);
-        });
-        // res.send({message:"create a new post in the database"});
-    })
-
-    .get(function(req, res){
-
-        //get all the posts in the database
-        Post.find(function(err,posts){
-            if(err){
-                return res.send(500, err);
-            }
-            return res.send(posts);
-        });
-        // res.send({message:"get all the posts in the database"});
-    })
-
-//api for a specfic post
+//post-specific commands. likely won't be used
 router.route('/posts/:id')
+	//gets specified post
+	.get(function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
+			res.json(post);
+		});
+	}) 
+	//updates specified post
+	.put(function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
 
-    //create
-    .put(function(req,res){
-        Post.findById(req.params.id, function(err, post){
-            if(err)
-                res.send(err);
+			post.created_by = req.body.created_by;
+			post.text = req.body.text;
 
-            post.created_by = req.body.created_by;
-            post.text = req.body.text;
+			post.save(function(err, post){
+				if(err)
+					res.send(err);
 
-            post.save(function(err, post){
-                if(err)
-                    res.send(err);
-
-                res.json(post);
-            });
-        });
-        // return res.send({message:'modify an existing post by using param ' + req.param.id});
-    })
-
-    .get(function(req,res){
-        Post.findById(req.params.id, function(err, post){
-            if(err)
-                res.send(err);
-            res.json(post);
-        });
-        // return res.send({message:'get an existing post by using param ' + req.param.id});
-    })
-
-    .delete(function(req,res){
-        Post.remove({
-            _id: req.params.id
-        }, function(err) {
-            if (err)
-                res.send(err);
-            res.json("Post is deleted !");
-        });
-        // return res.send({message:'delete an existing post by using param ' + req.param.id})
-    });
+				res.json(post);
+			});
+		});
+	})
+	//deletes the post
+	.delete(function(req, res) {
+		Post.remove({
+			_id: req.params.id
+		}, function(err) {
+			if (err)
+				res.send(err);
+			res.json("deleted :(");
+		});
+	});
 
 module.exports = router;
