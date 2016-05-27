8fdb7d87860050c06fe05d42aeb69a731d33a60e
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-27 19:01:42 -0400

    parsing json data

--- yelp/app.js
+++ yelp/app.js
@@ -10,78 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-/*
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
-console.log('Hello')
-/*
-var merge = require('merge');
-var yelp = require('node-yelp-api');
- 
-var options = {
-  consumer_key: 'rUpTzr4ffrUp3zSHeX9yFQ',
-  consumer_secret: 'p83Ea7afNax63n2pC6j33dUw34Y',
-  token: 'eEeXhE6noM20VnLJ4tc3NlwW_tWdEtNr',
-  token_secret: 'p4NOLqXsjgN7IYJ25sEvR52pLso',
-};
- 
-// See http://www.yelp.com/developers/documentation/v2/search_api 
-var parameters = {
-  term: 'pizza',
-  location: 'Raleigh',
-};
-yelp.search(merge(options, parameters), (data) => {
-  console.log(data);
-}, (err) => {
-  console.error(err);
-});
-*/
-
-
 require("yelp")
 var Yelp = require('yelp');
 var yelp = new Yelp({
@@ -94,7 +22,16 @@ var yelp = new Yelp({
 // See http://www.yelp.com/developers/documentation/v2/search_api
 yelp.search({ term: 'food', location: 'Montreal' })
 .then(function (data) {
-  console.log(data);
+  // console.log(data);
+  var jsonString = JSON.stringify(data);
+  jsonBussObj = JSON.parse(jsonString).businesses;
+  // console.log(jsonBussObj);
+  var l = jsonBussObj.length;
+  for(var i = 0; i<l;i++){
+    // var bussinessString = JSON.stringify(bussObj);
+    console.log(jsonBussObj[i]);
+  }
+
 })
 .catch(function (err) {
   console.error(err);
