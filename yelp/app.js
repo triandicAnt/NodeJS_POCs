2a9ce5a9182208870e48631df6ec792bad8a7e48
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-27 20:01:39 -0400

    saving business objects to mongodb database

--- yelp/app.js
+++ yelp/app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Connect to MongoDB
+require('./models/models');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yelp');  
+var Business = mongoose.model('Business');
+
+
 require("yelp")
 var Yelp = require('yelp');
 var yelp = new Yelp({
@@ -19,7 +26,6 @@ var yelp = new Yelp({
   token_secret: 'p4NOLqXsjgN7IYJ25sEvR52pLso',
 });
 
-// See http://www.yelp.com/developers/documentation/v2/search_api
 yelp.search({ term: 'food', location: 'Montreal' })
 .then(function (data) {
   // console.log(data);
@@ -28,12 +34,41 @@ yelp.search({ term: 'food', location: 'Montreal' })
   // console.log(jsonBussObj);
   var l = jsonBussObj.length;
   for(var i = 0; i<l;i++){
-    // var bussinessString = JSON.stringify(bussObj);
-    console.log(jsonBussObj[i]);
+    var bussiObj = jsonBussObj[i];
+    var newBusiness = new Business();
+
+    newBusiness.is_claimed  = bussiObj.is_claimed;
+    newBusiness.rating  = bussiObj.rating;
+    newBusiness.mobile_url = bussiObj.mobile_url;
+    newBusiness.rating_img_url = bussiObj.rating_img_url;
+    newBusiness.review_count = bussiObj.review_count;
+    newBusiness.name = bussiObj.bussiObj;
+    newBusiness.rating_img_url_small = bussiObj.rating_img_url_small;
+    newBusiness.url = bussiObj.url;
+    newBusiness.categories = bussiObj.categories;
+    newBusiness.phone = bussiObj.phone;
+    newBusiness.snippet_text = bussiObj.snippet_text;
+    newBusiness.image_url = bussiObj.image_url;
+    newBusiness.snippet_image_url = bussiObj.snippet_image_url;
+    newBusiness.display_phone = bussiObj.display_phone;
+    newBusiness.rating_img_url_large = bussiObj.rating_img_url_large;
+    newBusiness.id = bussiObj.id;
+    newBusiness.is_closed = bussiObj.is_closed;
+    newBusiness.location = bussiObj.location;
+
+    // save the user
+    newBusiness.save(function(err) {
+        if (err){
+            console.log('Error in Saving user: '+err);  
+            throw err;  
+        }
+    });
   }
+  console.log('Done saving to database.');
 
 })
 .catch(function (err) {
   console.error(err);
 });
+
 module.exports = app;
--- yelp/models/models.js
+++ yelp/models/models.js
@@ -1,1 +1,27 @@
 var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// Defining a schema for Business
+var businessSchema = new mongoose.Schema({
+    is_claimed : Boolean,
+    rating : String,
+    mobile_url: String,
+    rating_img_url: String,
+    review_count: String,
+    name: String,
+    rating_img_url_small: String,
+    url: String,
+    categories:Object,
+    phone: String,
+    snippet_text: String,
+    image_url: String,
+    snippet_image_url: String,
+    display_phone: String,
+    rating_img_url_large: String,
+    id: String,
+    is_closed: Boolean,
+    location:Object 
+});
+
+mongoose.model('Business', businessSchema);
